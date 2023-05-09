3c3b0ed1deddc6b2ba4ba72a468e30a8849f24d9
chiro45 <luciano.chiroli45@gmail.com>
2023-05-09 20:06:09 -0300

    refactor

--- src/components/ui/NavBarMobile/NavBarMobile.tsx
+++ src/components/ui/NavBarMobile/NavBarMobile.tsx
@@ -2,31 +2,33 @@
 import "./NavBarMobile.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faShop, faList, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
-export const NavBarMobile = () => {
 
+export const NavBarMobile = () => {
   return (
-    <div className="navBarMobile__containerPrincipal">
-      <div className="navBarMobile__contaierItems">
-        {
-          [{
-            text: "Home", icon: faHome, active: false
-          }, {
-            text: "Store", icon: faShop, active: true
-          }, {
-            text: "Ordenes", icon: faList, active: false
-          }, {
-            text: "cart", icon: faShoppingCart, active: false
-          }].map(({ text, icon, active }) => (
-            <div className={active ? "navBarCarItemActive" : "navBarCarItem"}>
-              <div>
-                <FontAwesomeIcon icon={icon} />
+    <div className="containerPrincipalNavBarMobile">
+      <div className="navBarMobile__containerPrincipal">
+        <div className="navBarMobile__contaierItems">
+          {
+            [{
+              text: "Home", icon: faHome, active: false
+            }, {
+              text: "Store", icon: faShop, active: true
+            }, {
+              text: "Ordenes", icon: faList, active: false
+            }, {
+              text: "cart", icon: faShoppingCart, active: false
+            }].map(({ text, icon, active }) => (
+              <div key={text} className={active ? "navBarCarItemActive" : "navBarCarItem"}>
+                <div>
+                  <FontAwesomeIcon icon={icon} />
+                </div>
+                <div>
+                  <p>{text}</p>
+                </div>
               </div>
-              <div>
-                <p>{text}</p>
-              </div>
-            </div>
-          ))
-        }
+            ))
+          }
+        </div>
       </div>
     </div>
   )
