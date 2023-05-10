15a4ce44503bc598ac92887a5a183c342abde60a
chiro45 <luciano.chiroli45@gmail.com>
2023-05-10 19:27:19 -0300

    nueva ruta

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -3,11 +3,13 @@ import { Routes, Route } from "react-router-dom"
 import { Landing } from "../components/Screens/Landing/Landing"
 import { ConfigCategory } from "../components/Screens/ConfigCategory/ConfigCategory"
 import "../styles/Global.css"
+import { ConfigArticuloInsumo } from "../components/Screens/ConfigArticuloInsumo/ConfigArticuloInsumo"
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
       <Route path="/config/category" element={<ConfigCategory/>}/>
+      <Route path="/config/articuloInsumo" element={<ConfigArticuloInsumo/>}/>
     </Routes>
   )
 }
