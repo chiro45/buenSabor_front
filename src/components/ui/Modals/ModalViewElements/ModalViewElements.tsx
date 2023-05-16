f8aff54d71262dff8f0871cd3c2f35add5076310
chiro45 <luciano.chiroli45@gmail.com>
2023-05-15 22:01:19 -0300

    entidades

--- src/components/ui/Modals/ModalViewElements/ModalViewElements.tsx
+++ src/components/ui/Modals/ModalViewElements/ModalViewElements.tsx
@@ -3,27 +3,45 @@ import { useSelector } from "react-redux"
 import { removeElementActiveTable } from "../../../../Redux/Reducers/TableReducer/TableReducer"
 import { handleModalsTable } from "../../../../Redux/Reducers/ModalsReducer/ModalsReducer"
 import { LayoutModal } from "../ModalTables/LayoutModal/LayoutModal"
+import { UnidadMedida } from "../../../Screens/ConfigArticuloInsumo/ConfigArticuloInsumo"
+import { ArticuloInsumo, ArticuloManufacturado, Producto } from "../../../../interfaces/entidades"
 
+
+interface Props {
+  object: UnidadMedida | ArticuloInsumo | Producto | ArticuloManufacturado;
+}
 
 export const ModalViewElements = () => {
 
   const openModal = useSelector((state: any) => state.ModalsReducer.modalview)
 
-  const elemetActive = useSelector((state: any) => state.TableReducer.elementActive)
+  const elemetActive: Props = useSelector((state: any) => state.TableReducer.elementActive)
 
   const dispatch = useDispatch()
   const handleCloseModal = () => {
     dispatch(handleModalsTable("modalview"))
     dispatch(removeElementActiveTable())
   }
-  console.log(elemetActive)
+
+  const getObjectProperties = () => {
+    return Object.entries(elemetActive).map(([key, value]) => (
+      <li key={key}>
+        <strong>{key}: </strong>
+        {value}
+      </li>
+    ));
+  };
   return (
     <div>
       {
         openModal === true
           ? <LayoutModal>
             <div>
-
+              <div>
+                <ul>
+                  {getObjectProperties()}
+                </ul>
+              </div>
               <button onClick={() => { handleCloseModal() }}>Cerrar</button>
             </div>
           </LayoutModal>
