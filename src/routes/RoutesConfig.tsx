fe798b3cd272ddc7b37cbfc8901ac6b9514e483c
FedericoPitton <fedejpitton@gmail.com>
2023-06-06 18:30:40 -0300

    Pruebas de Auth0

--- src/routes/RoutesConfig.tsx
+++ src/routes/RoutesConfig.tsx
@@ -2,6 +2,7 @@ import { Landing } from "../components/Screens/Landing/Landing"
 import { ConfigCategory } from "../components/Screens/ConfigCategory/ConfigCategory"
 import { ConfigArticuloInsumo } from "../components/Screens/ConfigArticuloInsumo/ConfigArticuloInsumo"
 import { ConfigUnidadMedida } from "../components/Screens/ConfigUnidadMedida/ConfigUnidadMedida"
+import { PruebaAuth0 } from "../components/Screens/PruebaAuth0/PruebaAuth0";
 
 export const routesConfig = [
   {
@@ -19,5 +20,9 @@ export const routesConfig = [
   {
     path: '/config/unidadMedida',
     component: ConfigUnidadMedida,
+  },
+  {
+    path: '/login',
+    component: PruebaAuth0,
   },
 ];
