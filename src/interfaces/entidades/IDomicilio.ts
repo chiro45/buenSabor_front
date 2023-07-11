9ab32dec1266307b52389dc82b45067afb181c57
FedericoPitton <fedejpitton@gmail.com>
2023-07-11 18:52:24 -0300

    Agrego nuevas interfaces

---
+++ src/interfaces/entidades/IDomicilio.ts
@@ -0,0 +1,10 @@
+export interface IDomicilio {
+    id: number;
+    tipoClase: string;
+    calle: string;
+    localidad: string;
+    numero: number;
+    departamento: string;
+    piso: number;
+  }
+  
---
+++ src/interfaces/entidades/IUsuario.ts
@@ -0,0 +1,6 @@
+export interface IUsuario {
+    id: number;
+    idAuth0: string;
+    usuario: string;
+    rol: string;
+  }
