fcce387bb10f43717c807753c9ea77bf68d0698d
FedericoPitton <fedejpitton@gmail.com>
2023-07-17 10:57:07 -0300

    cambio y agrego estilos a cocina

--- src/components/ui/Kitchen/CardKitchen/CardKitchen.tsx
+++ src/components/ui/Kitchen/CardKitchen/CardKitchen.tsx
@@ -13,7 +13,7 @@ const CardKitchen = ({ pedido }: CardKitchenProps) => {
 
     const PENDIENTE = "#FF7700"; // naranja
     const PREPARADO = "#1DD75B"; // verde
-    const PREPARACION = "#00BDD6";
+    const PREPARACION = "#FFCC32"; //amarillo
     const RECHAZADO = "#F22128";
     const [colorState, setColorState] = useState<string | undefined>(undefined);
     const [btnState, setbtnState] = useState<string>()
@@ -41,8 +41,15 @@ const CardKitchen = ({ pedido }: CardKitchenProps) => {
                         <h4 className='cardKitchen_order' style={{ color: colorState }}>#{pedido.id}</h4>
                     </div>
                     <div className='cardKitchen_header-right'>
-                        <p className='cardKitchen_clock' style={{ background: colorState }}><FontAwesomeIcon icon={faClock} /> 23:40</p>
-                        <h4 className='cardKitchen_state' style={{ background: colorState }}>{pedido.estadoPedido === 'PENDIENTE' ? 'EN ESPERA' : pedido.estadoPedido}</h4>
+                        <p className='cardKitchen_clock' style={{ background: colorState }}>
+                            <FontAwesomeIcon icon={faClock} />
+                            23:40
+                        </p>
+                        <h4
+                            className='cardKitchen_state'
+                            style={{ background: colorState }}>
+                            {pedido.estadoPedido === 'PENDIENTE' ? 'EN ESPERA' : pedido.estadoPedido}
+                        </h4>
                     </div>
                 </div>
 
@@ -63,7 +70,7 @@ const CardKitchen = ({ pedido }: CardKitchenProps) => {
                             pointerEvents: pedido.estadoPedido === 'RECHAZADO' || pedido.estadoPedido === 'PREPARADO' ? 'none' : 'auto',
                             opacity: pedido.estadoPedido === 'RECHAZADO' || pedido.estadoPedido === 'PREPARADO' ? 0.5 : 1,
                         }}
-                        onClick={()=>{console.log('ACA va el update del pedido')}}
+                        onClick={() => { console.log('ACA va el update del pedido') }}
                     >
                         {btnState}
                     </button>
