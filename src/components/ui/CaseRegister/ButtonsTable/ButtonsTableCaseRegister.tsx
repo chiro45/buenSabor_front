4751d6b3f544ad5843dee2e8b7d2460b662db705
chiro45 <luciano.chiroli45@gmail.com>
2023-07-19 22:43:41 -0300

    funciones de la caja

--- src/components/ui/CaseRegister/ButtonsTable/ButtonsTableCaseRegister.tsx
+++ src/components/ui/CaseRegister/ButtonsTable/ButtonsTableCaseRegister.tsx
@@ -1,26 +1,62 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import './ButtonsTableCaseRegister.css'
+import { EEstadoPedido, IPedido } from '../../../../interfaces';
+import { useAccessToken } from '../../../../hooks';
+import { cancelOrder, deliverOrder, payOrder, sendToKitchen } from "../CaseRegisterTableFunctions";
+import { ViewProductDetail } from '../modal/ViewProductDetail/ViewProductDetail';
 interface ActionButton {
-    element: any,
+    element: IPedido,
     textButton: string;
     className: string;
-    fnOnclick: () => void;
+    conditions: EEstadoPedido[]
+    fnOnclick: 'viewDetail' | 'viewFacture' | 'sendtoKittchen' | 'payOrder' | 'cancelOrder' | 'deliverOrder';
+
 }
 
 
-export const ButtonsTableCaseRegister: FC<ActionButton> = ({ element, className, fnOnclick, textButton }) => {
-    
+export const ButtonsTableCaseRegister: FC<ActionButton> = ({ element, className, fnOnclick, textButton, conditions }) => {
+    const header = useAccessToken();
+
     const handleOnclickButton = () => {
-        fnOnclick()
-    }
-    const handleState = ()=>{
-        return className
+        if (fnOnclick === 'viewDetail') {
+            setProduct(element)
+        } else if (fnOnclick === 'cancelOrder') {
+            cancelOrder(element, header)
+        } else if (fnOnclick === 'payOrder') {
+            payOrder(element, header)
+        } else if (fnOnclick === 'deliverOrder') {
+            deliverOrder(element, header)
+
+        } else if (fnOnclick === 'sendtoKittchen') {
+            sendToKitchen(element, header)
+        }
     }
 
+    const handleState = () => {
+        const pedidoState = element.estadoPedido
+        if (conditions.includes(pedidoState) || className === 'btn__actionCaseRegister__viewFactureButton' && element.pagoConfirmado === true) {
+            return className
+        } else {
+            return "disabled"
+        }
+
+    }
+
+    const [product, setProduct] = useState<IPedido | null>(null)
 
     return (
         <div className='container__ButtonsTableCaseRegister'>
-            <button  key={element.numeroPedido}
+            {
+                product &&
+                <ViewProductDetail
+                    handleClose={() => { setProduct(null) }}
+                    pedido={product}
+                    width={'40vw'}
+                    height={'55vh'}
+                />
+            }
+
+            <button key={element.numero}
                 className={` ${handleState()}`}
                 onClick={handleOnclickButton}>
                 {textButton}
