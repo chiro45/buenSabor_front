f71161c54d24ea54897066a28d221b6e759df793
FedericoPitton <fedejpitton@gmail.com>
2023-07-22 14:08:33 -0300

    DropDown y header dinamicos al rol

--- src/components/ui/Header/Header.tsx
+++ src/components/ui/Header/Header.tsx
@@ -6,62 +6,74 @@ import { useNavigate } from "react-router-dom";
 import { useAccessToken, useInput } from "../../../hooks";
 import { useSelector } from "react-redux";
 import { useEffect } from "react";
-import { alertError } from "../../../functions/alerts";
 import DropdownLogin from "../DropdownLogin/DropdownLogin";
+import useUserRole from "../../../hooks/useUserRole";
+import { useAuth0 } from "@auth0/auth0-react";
+
 
 export const Header = () => {
-
   const [inputState, onInputChange, setInputState]: any = useInput({
-    busqueda: ""
-  })
+    busqueda: ''
+  });
 
-
-  const dispatch = useDispatch()
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+  const { rol, loading } = useUserRole();
+  const dispatch = useDispatch();
   const headers = useAccessToken();
-  const navigate = useNavigate()
-  const {busqueda, orderPriceActive} = useSelector((state: any) => state.StoreProductReducer)
-  
+  const navigate = useNavigate();
+  const { busqueda, orderPriceActive } = useSelector((state: any) => state.StoreProductReducer);
+
   useEffect(() => {
-    onSearched()
+    onSearched();
   }, [busqueda]);
 
-  const onSearched = async() =>{
-    if(busqueda !== ""){
-      const url = `${import.meta.env.VITE_URL_ARTICULOMANUFACTURADO}/allByName/0/${orderPriceActive || 'default'}/${busqueda || 'default'}`
-      dispatch(startAddProductStore(url, headers)).then(() =>{ navigate('/store')})
-      
-     
+  const onSearched = async () => {
+    if (busqueda !== '') {
+      const url = `${import.meta.env.VITE_URL_ARTICULOMANUFACTURADO}/allByName/0/${orderPriceActive || 'default'}/${busqueda || 'default'}`;
+      dispatch(startAddProductStore(url, headers)).then(() => {
+        navigate('/store');
+      });
     }
-  }
+  };
 
   const handleSearch = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.keyCode === 13) {
-      if(inputState.busqueda !==""){
-        dispatch(addSearchActive(inputState.busqueda))
+      if (inputState.busqueda !== '') {
+        dispatch(addSearchActive(inputState.busqueda));
       }
     }
   };
- 
+
   return (
     <div className="containerHeaderPage__storePage">
-      <div className="containerImgLogo__storePage" onClick={()=>{
-        navigate('/')
-        dispatch(removeSearchActive())
-        }}>
+      <div className="containerImgLogo__storePage" onClick={() => {
+        navigate('/');
+        dispatch(removeSearchActive());
+      }}>
         <img src="/src/assets/logopng.webp" />
       </div>
-      <div className="containerInputStore__storePage">
-        <input name='busqueda'
-          onKeyDown={handleSearch}
-          value={inputState.busqueda}
-          onChange={onInputChange}
-          className="InputStore__storePage"
-          type="text"
-          placeholder="Busca un producto" />
-      </div>
+      {loading ? (
+        <h4>Cargando...</h4> // Muestra 'Cargando...' mientras se obtiene el rol
+      ) : (
+        <div className="containerInputStore__storePage">
+          {isAuthenticated && (rol === 'CLIENTE' || rol === 'CAJERO') ? (
+            <input
+              name="busqueda"
+              onKeyDown={handleSearch}
+              value={inputState.busqueda}
+              onChange={onInputChange}
+              className="InputStore__storePage"
+              type="text"
+              placeholder="Busca un producto"
+            />
+          ) : (
+            <h4 style={{color:'white'}}>{rol}</h4> // Muestra el rol si no es CLIENTE ni COCINERO o no está autenticado
+          )}
+        </div>
+      )}
       <div className="containerActionsStore__storePage">
-        <DropdownLogin/>
+        <DropdownLogin />
       </div>
     </div>
   );
-}
+};
