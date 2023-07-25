a0f4575b5da2b66985cf14bc6e42474d4e36ea78
FedericoPitton <fedejpitton@gmail.com>
2023-07-25 15:27:13 -0300

    agrego header standar a delivery

--- src/components/Screens/DeliveryView/Deliveryview.tsx
+++ src/components/Screens/DeliveryView/Deliveryview.tsx
@@ -3,6 +3,7 @@ import img from '../../../assets/logopng.webp'
 import { useState } from 'react';
 import { Footer } from '../../ui/Footer/Footer';
 import { ItemDelivery } from './ItemOrdersDelivery/ItemOrdersDelivery';
+import { Header } from '../../ui';
 export const Deliveryview = () => {
 
     const [isChecked, setIsChecked] = useState(false);
@@ -14,47 +15,48 @@ export const Deliveryview = () => {
 
     return (
         <div className='containerDeliveryView'>
-                <div className='headerDelivery'>
-                    <div className='nameHeader'><p>Federico</p></div>
-                    <div className='imgHeader'><img  src={img} /></div>
+            {/* <div className='headerDelivery'>
+                <div className='nameHeader'><p>Federico</p></div>
+                <div className='imgHeader'><img src={img} /></div>
                 `   <div className='buttonAccountHeader'><button>Mi cuenta</button></div>
-                </div>
+            </div> */}
+            <Header/>
             <div className='containerBodyDelivery'>
-                
-                    <div className='containerPadding'>
-                    <div className={`containerCheckDisponible ${isChecked ? 'disponible': 'noDisponible'}`}>
-                            <div>
-                                <h3>{isChecked ? 'Disponible' : 'No Disponible'}</h3>
-                            </div>
-                            <div>
-                                <label className="switch">
-                                    <input type="checkbox" checked={isChecked} onChange={handleChange} />
-                                    <span className="slider round" />
-                                </label>
-                            </div>
-                       </div>
-                      
+
+                <div className='containerPadding'>
+                    <div className={`containerCheckDisponible ${isChecked ? 'disponible' : 'noDisponible'}`}>
                         <div>
-                            <h3>Pedidos</h3>
+                            <h3>{isChecked ? 'Disponible' : 'No Disponible'}</h3>
                         </div>
-                        <div className='containerPedidos'>
-                            <div className="containerHeaderPedidos">
-                                <div><p>Nº</p></div>
-                                <div><p>Entregado</p></div>
-                                <div><p>Detalle</p></div>
-                                <div><p>Direccion</p></div>
-                            </div>
-                            <div className='containerPedidosBody'>
-                                {
+                        <div>
+                            <label className="switch">
+                                <input type="checkbox" checked={isChecked} onChange={handleChange} />
+                                <span className="slider round" />
+                            </label>
+                        </div>
+                    </div>
+
+                    <div>
+                        <h3>Pedidos</h3>
+                    </div>
+                    <div className='containerPedidos'>
+                        <div className="containerHeaderPedidos">
+                            <div><p>Nº</p></div>
+                            <div><p>Entregado</p></div>
+                            <div><p>Detalle</p></div>
+                            <div><p>Direccion</p></div>
+                        </div>
+                        <div className='containerPedidosBody'>
+                            {
                                 [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}].map(() => (
-                                        <ItemDelivery />
-                                    ))
-                                }
-                            </div>
+                                    <ItemDelivery />
+                                ))
+                            }
                         </div>
+                    </div>
                 </div>
             </div>
-            <Footer/>
+            <Footer />
         </div>
     )
 }
