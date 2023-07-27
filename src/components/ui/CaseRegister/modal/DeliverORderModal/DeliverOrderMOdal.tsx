665c055352fc5851627d61239b8cff4c0c78b882
chiro45 <luciano.chiroli45@gmail.com>
2023-07-26 23:55:45 -0300

    delivery

--- src/components/ui/CaseRegister/modal/DeliverORderModal/DeliverOrderMOdal.tsx
+++ src/components/ui/CaseRegister/modal/DeliverORderModal/DeliverOrderMOdal.tsx
@@ -1,37 +1,74 @@
 
-import { EEstadoPedido, ETipoEnvio, IPedido } from '../../../../../interfaces';
+import { EEstadoPedido, ETipoEnvio, IPedido, IUsuario } from '../../../../../interfaces';
 import { LayoutModalCaseRegister } from '../Layout/LayoutModalCaseRegister'
-import { FC, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 
 import './DeliveryOrderModal.css'
-import { handleUpdateState } from '../../CaseRegisterTableFunctions';
+import { handleUpdateState, handleUpdateStateEnvio } from '../../CaseRegisterTableFunctions';
 import { useAuth0 } from '@auth0/auth0-react';
-export const DeliverOrderMOdal: FC<{ modalEnvio: IPedido, setModalEnvio: Function }> = ({ modalEnvio, setModalEnvio }) => {
-    const { getAccessTokenSilently } = useAuth0()
-   
-    const handleEnvioDelivery = async() => {
+import { alertConfirm, alertSuccess } from '../../../../../functions/alerts';
+import { fetchGet } from '../../../../../helpers';
+
+const urlrolDelivery = `${import.meta.env.VITE_URL_USUARIO}/getByRol/${ETipoEnvio.DELIVERY}`
+export const DeliverOrderMOdal: FC<{ pedido: IPedido, setModalEnvio: Function }> = ({ pedido: modalEnvio, setModalEnvio }) => {
+
+    const { getAccessTokenSilently, user } = useAuth0()
+
+    const [deliverys, setDeliverys] = useState<IUsuario[]>([])
+
+    const [optionSelected, setOptionSelected] = useState<IUsuario>()
+    const handleDeliverys = async () => {
         const token = await getAccessTokenSilently();
-        const headers = {
+        fetchGet(urlrolDelivery, {
             'Authorization': `Bearer ${token}`
-        };
-        handleUpdateState(EEstadoPedido.CAMINO, modalEnvio, headers)
-        setModalEnvio(null)
+        }).then(data => {
+            setDeliverys(data)
+        })
+
     }
-    const handleRetiroEnLocal = async () => {
-        const token = await getAccessTokenSilently();
-        const headers = {
-            'Authorization': `Bearer ${token}`
-        };
-        handleUpdateState(EEstadoPedido.ENTREGADO, modalEnvio, headers)
-        setModalEnvio(null)
+    useEffect(() => {
+        handleDeliverys()
+    }, [])
+
+    const handleEnvioDelivery = async () => {
+        alertConfirm('¿Entregar pedido?',
+            'Confirmar Entrega',
+            'Si',
+            async () => {
+                const token = await getAccessTokenSilently();
+                const headers = {
+                    'Authorization': `Bearer ${token}`
+                };
+                if (optionSelected) handleUpdateStateEnvio(EEstadoPedido.CAMINO, modalEnvio, headers, optionSelected.idAuth0)
+                alertSuccess("Pedido Entregado", 'Correcto')
+                setModalEnvio(null)
+            },
+            'No')
+
     }
 
-    const [optionSelected, setOptionSelected] = useState('')
+    const handleRetiroEnLocal = async () => {
+        alertConfirm('¿Entregar pedido?',
+            'Confirmar Entrega',
+            'Si',
+            async () => {
+                const token = await getAccessTokenSilently();
+                const headers = {
+                    'Authorization': `Bearer ${token}`
+                };
+                const idAuth0 = user?.sub?.split('|').pop();
+                if (idAuth0) handleUpdateStateEnvio(EEstadoPedido.CAMINO, modalEnvio, headers, idAuth0)
+                alertSuccess("Pedido Entregado", 'Correcto')
+                setModalEnvio(null)
+            },
+            'No')
+
+    }
 
     const onchangeSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
         const selectedOption = e.target.value;
-        setOptionSelected(selectedOption);
-
+        const result = deliverys.find(el => el.usuario === selectedOption)
+        setOptionSelected(result);
     }
 
     return (
@@ -44,36 +81,40 @@ export const DeliverOrderMOdal: FC<{ modalEnvio: IPedido, setModalEnvio: Functio
                 </div>
                 {
                     modalEnvio.tipoEnvio === ETipoEnvio.LOCAL &&
-                    <div>
-                        <button onClick={handleRetiroEnLocal} className='buttonEntregaEnLocal'>Entrega a cliente en local</button>
+                    <div style={{ width: '100%', height: '50%', display: 'flex', flexDirection: 'column', justifyContent: 'space-around', alignItems: 'center' }}>
+                        <div>
+                            <button onClick={handleRetiroEnLocal} className='buttonEntregaEnLocal'>Entrega a cliente en local</button>
+                        </div>
+                        <div className='containerCloseMOdalDeliverty'>
+                            <button onClick={() => { setModalEnvio(null) }}>Cerrar</button>
+                        </div>
                     </div>
                 }
 
                 {
                     modalEnvio.tipoEnvio === ETipoEnvio.DELIVERY &&
-                    <div className='containerEscogeDelivery'>
-                        <h2>Escoge Delivery</h2>
+                    <div className="containerSleectDelivery">
                         <div>
-                            <select value={optionSelected} onChange={onchangeSelect}>
-                                <option>Juancarlos</option>
-                                <option>Juancarlos</option>
-                                <option>Juancarlos</option>
-                                <option>Juancarlos</option>
-                            </select>
+                            <h1>Selecciona el delivery</h1>
                         </div>
-
-                        <div className='containerConfirmDelivery'>
-                            <div>
-                                <button onClick={handleEnvioDelivery}>Confirmar</button>
-                            </div>
+                        <div className="containerSelect">
+                            <select value={optionSelected?.usuario || ''} onChange={onchangeSelect}>
+                                {deliverys.map((option) => (
+                                    <option key={option.usuario} value={option.usuario}>
+                                        {option.usuario}
+                                    </option>
+                                ))}
+                            </select >
+                        </div>
+                        <div className="containerButtonsSelectDelivery">
+                            <button className="confirm" onClick={handleEnvioDelivery}>Confirmar</button>
+                            <button className="cancel" onClick={() => { setModalEnvio(null) }}>Cancelar</button>
                         </div>
                     </div>
 
                 }
 
-                <div className='containerCloseMOdalDeliverty'>
-                    <button onClick={() => { setModalEnvio (null)}}>Cerrar</button>
-                </div>
+
             </div>
         </LayoutModalCaseRegister>
     )
--- src/interfaces/entidades/IPedido.ts
+++ src/interfaces/entidades/IPedido.ts
@@ -2,12 +2,14 @@ import { EEstadoPedido, EFormaPago, ETipoEnvio } from "../enums";
 import { ICliente } from "./ICliente";
 import { IDetallePedido } from "./IDetallePedido";
 import { IDomicilio } from "./IDomicilio";
+import { IUsuario } from "./IUsuario";
 
 export interface IPedido {
   id: number;
   tipoClase: string;
   fecha: string;
   numero: number;
+  usuarioEntrega: IUsuario
   monto: number;
   pagoConfirmado: boolean;
   domicilio: IDomicilio;
