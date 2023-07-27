3670ed33d8ace6e9379084af08ebed0de9a3a53b
chiro45 <luciano.chiroli45@gmail.com>
2023-07-27 14:27:59 -0300

    add carrito

--- src/components/ui/DropdownLogin/DropdownLogin.tsx
+++ src/components/ui/DropdownLogin/DropdownLogin.tsx
@@ -31,6 +31,7 @@ const DropdownLogin = () => {
                 <Link className='dropdown_option' to='/profile'>Perfil</Link>
                 <Link className='dropdown_option' to='/address'>Domicilio</Link>
                 <Link className='dropdown_option' to='/order'>Pedidos</Link>
+                <Link className='dropdown_option' to='/cart'>Carrito</Link>
                 <a className='dropdown_option' onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
                   Cerrar Sesión
                 </a>
